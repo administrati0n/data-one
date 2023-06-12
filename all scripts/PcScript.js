0cc428a74f845bed6d32791c2807140d02831d6f
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-06-13 00:26:40 +0300

    Update PcScript.js

--- all scripts/PcScript.js
+++ all scripts/PcScript.js
@@ -1,30 +1,44 @@
 // Libreries
 const lib = document.createElement("script");
-lib.src = "lib.js";
+lib.src = "https://administrati0n.github.io/data-one/all scripts/lib.js";
 document.head.appendChild(lib);
 
 // Fonts
 const fonts = document.createElement("script");
-fonts.src = "fonts.js";
+fonts.src = "https://administrati0n.github.io/data-one/all scripts/fonts.js";
 document.head.appendChild(fonts);
-
-// iconLinks
-const iconLinks = document.createElement("script");
-iconLinks.src = "iconLinks.js";
-document.head.appendChild(iconLinks);
 
 // Variables
 const Stuff1 = document.createElement("script");
-Stuff1.src = "Stuff1.js";
+Stuff1.src = "https://administrati0n.github.io/data-one/all scripts/Stuff1.js";
 document.body.appendChild(Stuff1);
+
+// iconLinks2
+const iconLinks = document.createElement("script");
+iconLinks2.src = "https://administrati0n.github.io/data-one/all scripts/Content/iconLinks2.js";
+document.head.appendChild(iconLinks2);
 
 // Content
 const Content = document.createElement("script");
-Content.src = "content.js";
+Content.src = "https://administrati0n.github.io/data-one/all scripts/Content/content.js";
 document.body.appendChild(Content);
 
 // Pinned
 const Pin = document.createElement("script");
-Pin.src = "Pin.js";
+Pin.src = "https://administrati0n.github.io/data-one/all scripts/Pin/Pin.js";
 document.body.appendChild(Pin);
-console.log("1");
+
+// iconLinks
+const iconLinks = document.createElement("script");
+iconLinks.src = "https://administrati0n.github.io/data-one/all scripts/Content/iconLinks.js";
+document.head.appendChild(iconLinks);
+
+// WorkScripts
+const WorkScripts = document.createElement("script");
+WorkScripts.src = "https://administrati0n.github.io/data-one/all scripts/WorkScripts/WorkScripts.js";
+document.body.appendChild(WorkScripts);
+
+// Device
+const Device = document.createElement("script");
+Device.src = "https://administrati0n.github.io/data-one/all scripts/WorkScripts/Device.js";
+document.body.appendChild(Device);
