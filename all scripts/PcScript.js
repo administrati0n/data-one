c8b56a3fce24c10b61e174cc22de322fefa83a3e
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-06-13 00:16:45 +0300

    Add files via upload

---
+++ all scripts/PcScript.js
@@ -0,0 +1,30 @@
+// Libreries
+const lib = document.createElement("script");
+lib.src = "lib.js";
+document.head.appendChild(lib);
+
+// Fonts
+const fonts = document.createElement("script");
+fonts.src = "fonts.js";
+document.head.appendChild(fonts);
+
+// iconLinks
+const iconLinks = document.createElement("script");
+iconLinks.src = "iconLinks.js";
+document.head.appendChild(iconLinks);
+
+// Variables
+const Stuff1 = document.createElement("script");
+Stuff1.src = "Stuff1.js";
+document.body.appendChild(Stuff1);
+
+// Content
+const Content = document.createElement("script");
+Content.src = "content.js";
+document.body.appendChild(Content);
+
+// Pinned
+const Pin = document.createElement("script");
+Pin.src = "Pin.js";
+document.body.appendChild(Pin);
+console.log("1");
---
+++ all scripts/fonts.js
@@ -0,0 +1,18 @@
+const fontStyles = `
+  <style type="text/css">
+    @font-face {
+      font-family: Gilroy;
+      src: url('https://administrati0n.github.io/data-one/fonts/Font2.ttf');
+    }
+    @font-face {
+      font-family: "SF Pro Semibold Rails";
+      src: url('SF Pro - Semibold Rails.otf');
+    }
+    @font-face {
+      font-family: "SF Pro Display Medium";
+      src: url('Original Apple Fonts/SF Pro Display/SF-Pro-Display-Medium.otf');
+    }
+  </style>
+`;
+
+document.head.insertAdjacentHTML('beforeend', fontStyles);
