c0e2fee2b14d364d5e53545f68ffd8b690a25166
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-06-13 00:29:41 +0300

    Update fonts.js

--- all scripts/fonts.js
+++ all scripts/fonts.js
@@ -2,15 +2,15 @@ const fontStyles = `
   <style type="text/css">
     @font-face {
       font-family: Gilroy;
-      src: url('https://administrati0n.github.io/data-one/fonts/Font2.ttf');
+      src: url('https://administrati0n.github.io/data-one/All Fonts/Font2.ttf');
     }
     @font-face {
       font-family: "SF Pro Semibold Rails";
-      src: url('SF Pro - Semibold Rails.otf');
+      src: url('https://administrati0n.github.io/data-one/All Fonts/SF Pro - Semibold Rails.otf');
     }
     @font-face {
       font-family: "SF Pro Display Medium";
-      src: url('Original Apple Fonts/SF Pro Display/SF-Pro-Display-Medium.otf');
+      src: url('https://administrati0n.github.io/data-one/All Fonts/SF-Pro-Display-Medium.otf');
     }
   </style>
 `;
